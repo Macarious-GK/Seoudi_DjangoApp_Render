7f28d3b8f1deb3ec6d963c104a7ccf51a05ecc16
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2023-08-08 00:12:25 +0300

    Fixing

--- Ordering/urls.py
+++ Ordering/urls.py
@@ -5,4 +5,6 @@ from Ordering import views
 
 urlpatterns = [
     path('',views.order, name = 'order'),
+    path('customers/',views.customer_CR, name= 'customers'),
+    path('customers/<int:pk>/',views.customer_UD, name= 'customersid'),
 ]
