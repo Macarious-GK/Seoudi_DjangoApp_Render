9cc249eff19092183a54e5370528ff543dfd6e47
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2023-08-08 13:07:32 +0300

    adding orders and items and status

--- Ordering/urls.py
+++ Ordering/urls.py
@@ -6,4 +6,8 @@ from Ordering import views
 urlpatterns = [
     path('customers/',views.customer_CR, name= 'customers'),
     path('customers/<int:pk>/',views.customer_UD, name= 'customersid'),
+    path('items/',views.item_CR, name= 'item-cr'),
+    path('items/<int:pk>/',views.item_UD, name= 'item-ud'),
+    path('orders/',views.order_CR, name= 'order-cr'),
+    path('orders/<int:pk>/',views.order_UD, name= 'order-ud'),
 ]
