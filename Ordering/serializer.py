71ddb912e61bedfdfcd4da13ae8922c197421cad
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2023-08-08 22:39:16 +0300

    chinging status

--- Ordering/serializer.py
+++ Ordering/serializer.py
@@ -14,8 +14,14 @@ class ItemSerializer(serializers.ModelSerializer):
         model = Order_Item
         fields = '__all__'
 
+class statusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderStatus
+        fields = ['name']
 
 class OrderSerializer(serializers.ModelSerializer):
+    status = statusSerializer(read_only = True)
+
     class Meta:
         model = Order_Details
         fields = '__all__'
